e09f1e528a9edd8f9c0b0f709b208e53eef9c1f1
Ribka Mengiste <167540456+Ribka12@users.noreply.github.com>
2025-12-29 18:55:26 +0300

    checkUser in authController added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,4 +95,14 @@ async function login(req, res) {
   }
 }
 
-module.exports = { register, login };
+async function checkUser(req, res) {
+  // Check user logic here
+
+  const user = req.user;
+  return res.status(StatusCodes.OK).json({
+    message: "Valid user",
+    userid: user.userid,
+    username: user.username,
+  });
+}
+module.exports = { register, login, checkUser };
